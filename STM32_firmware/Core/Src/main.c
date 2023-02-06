2f6660a7d7de672e36242c0b79bbcc8148d10781
KozAAAAA <mateusz-koza@outlook.com>
2023-02-06 23:54:37 +0100

    Set flag for wait-regulate-decelerate cycle

--- STM32_firmware/Core/Src/main.c
+++ STM32_firmware/Core/Src/main.c
@@ -42,7 +42,6 @@
 
 typedef enum
 {
-	WAITING_STATE,
 	REGULATION_STATE,
 	DECELERATION_STATE
 } ControllerStateTypeDef;
@@ -57,9 +56,8 @@ typedef enum
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 
-#define ACCEPTED_REGULATION_ERROR 5
 #define ACCEPTED_SPEED_RANGE 20
-#define DECELERATION_RATE 200
+#define DECELERATION_RATE 300
 
 /* USER CODE END PM */
 
@@ -69,12 +67,13 @@ typedef enum
 
 static float32_t encoder = 0;
 static float32_t setpoint = 0;
-static float32_t new_setpoint = 0;
 static float32_t angle_meas = 0;
 static float32_t acceleration = 0;
 static float32_t acceleration_filtered = 0;
 
-static ControllerStateTypeDef rwacs_state = WAITING_STATE;
+static ControllerStateTypeDef rwacs_state = REGULATION_STATE;
+
+volatile uint8_t new_cycle_flag = 0;
 
 /* USER CODE END PV */
 
@@ -91,59 +90,55 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
 	encoder = ENC_UpdateCounter(&henc1, GPIO_Pin);
 	if(ENC_OnButtonPress(&henc1, GPIO_Pin))
-		new_setpoint = encoder;
-
-	if(GPIO_Pin == USER_BUTTON_Pin){
+	{
+		setpoint = encoder;
 		rwacs_state = DECELERATION_STATE;
 	}
+
 /*  NOTE: Occupied GPIO lines: 12, 13, 10										  */
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
 	if(htim->Instance == TIM3){
-
-		MPU6050_GetYaw(&angle_meas);
-
-		switch(rwacs_state)
-		{
-			case WAITING_STATE:
-			{
-				if(setpoint != new_setpoint){
-					setpoint = new_setpoint;
-					rwacs_state = REGULATION_STATE;
-				}
-				break;
-			}
-			case REGULATION_STATE:
-			{
-				PID_Control(&hpid1, &setpoint, &angle_meas, &acceleration);
-				break;
-			}
-			case DECELERATION_STATE:
-			{
-				if(hdrv8825_1.Speed > ACCEPTED_SPEED_RANGE)
-				{
-					acceleration = -DECELERATION_RATE;
-				}
-				else if(hdrv8825_1.Speed < -ACCEPTED_SPEED_RANGE)
-				{
-					acceleration = DECELERATION_RATE;
-				}
-				else{
-					float32_t zero = 0;
-					DRV8825_SetSpeed(&hdrv8825_1, &zero);
-					acceleration = 0;
-					rwacs_state = WAITING_STATE;
-				}
-				break;
-			}
-		}
-
-		DX_Limit(&hdx1, &acceleration, &acceleration_filtered);
-		DRV8825_SetAcceleration(&hdrv8825_1, &acceleration_filtered);
-		RWACS_Print_Controller_State(&setpoint, &angle_meas, &hdrv8825_1.Speed, &acceleration);
+		new_cycle_flag = 1;
 	}
+}
+
+void wait_regulate_decelerate()
+{
+	MPU6050_GetYaw(&angle_meas);
+
+	switch(rwacs_state)
+	{
+		case REGULATION_STATE:
+		{
+			PID_Control(&hpid1, &setpoint, &angle_meas, &acceleration);
+			break;
+		}
+		case DECELERATION_STATE:
+		{
+			if(hdrv8825_1.Speed > ACCEPTED_SPEED_RANGE)
+			{
+				acceleration = -DECELERATION_RATE;
+			}
+			else if(hdrv8825_1.Speed < -ACCEPTED_SPEED_RANGE)
+			{
+				acceleration = DECELERATION_RATE;
+			}
+			else{
+				float32_t zero = 0;
+				DRV8825_SetSpeed(&hdrv8825_1, &zero);
+				acceleration = 0;
+				rwacs_state = REGULATION_STATE;
+			}
+			break;
+		}
+	}
+
+	DX_Limit(&hdx1, &acceleration, &acceleration_filtered);
+	DRV8825_SetAcceleration(&hdrv8825_1, &acceleration_filtered);
+	RWACS_Print_Controller_State(&setpoint, &angle_meas, &hdrv8825_1.Speed, &acceleration);
 }
 
 /* USER CODE END 0 */
@@ -197,6 +192,11 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  if(new_cycle_flag == 1)
+	  {
+		  wait_regulate_decelerate();
+		  new_cycle_flag = 0;
+	  }
 
     /* USER CODE END WHILE */
 
